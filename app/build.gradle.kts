5d141cb09f9f39537d01f0681d1d3726bcc1bbbd
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2025-12-30 15:03:05 -0800

    Converted Groovy gradle files to gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+import java.util.Properties
+
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    id ("com.apollographql.apollo")
+    id ("org.jetbrains.kotlin.plugin.serialization")
+}
+
+val localProps = Properties()
+val localPropsFile = File(rootProject.rootDir, "keys.properties")
+if (localPropsFile.exists() && localPropsFile.isFile) {
+    localPropsFile.inputStream().use {stream ->
+        localProps.load(stream)
+    }
+}
+
+android {
+    namespace = "com.example.coffeescout"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.coffeescout"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String", "API_KEY", localProps.getProperty("API_KEY"))
+        }
+
+        debug {
+            buildConfigField("String", "API_KEY", localProps.getProperty("API_KEY"))
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.example.coffeescout")
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+dependencies {
+    val lifecycleVersion = "2.10.0"
+
+    implementation ("io.coil-kt:coil:2.7.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation ("androidx.paging:paging-runtime:3.3.6")
+    implementation ("com.squareup.okhttp3:okhttp:5.3.2")
+    implementation ("com.apollographql.apollo:apollo-runtime:4.3.3")
+    implementation ("androidx.core:core-ktx:1.17.0")
+    implementation ("androidx.appcompat:appcompat:1.7.1")
+    implementation ("com.google.android.material:material:1.13.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.2.1")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.7.0")
+}
