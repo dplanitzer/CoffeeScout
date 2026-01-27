6cc0108058c8c60cb272bc834832e4e6fdc95b8a
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2026-01-27 14:03:49 -0800

    Added some padding at the top of the screen

--- app/src/main/java/com/example/coffeescout/MainScreen.kt
+++ app/src/main/java/com/example/coffeescout/MainScreen.kt
@@ -2,11 +2,13 @@ package com.example.coffeescout
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -18,7 +20,9 @@ fun MainScreen(viewModel: MainViewModel = viewModel(),
 ) {
     val lazyPagingItems = viewModel.businessFlow.collectAsLazyPagingItems()
 
-    LazyColumn {
+    LazyColumn(
+        modifier = Modifier.padding(top = 16.dp)
+    ) {
         items(lazyPagingItems.itemCount) { index ->
             val business = lazyPagingItems[index]
 
@@ -47,7 +51,7 @@ fun MainScreen(viewModel: MainViewModel = viewModel(),
                     ) }
                 }*/
                 loadState.refresh is LoadState.Error -> {
-                    val e = loadState.refresh as LoadState.Error
+                    //val e = loadState.refresh as LoadState.Error
                     item { ErrorView { viewModel.invalidate() } }
                 }
             }
