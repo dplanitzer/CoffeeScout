b3a0f560c72439acb906bf8823e9d35a575574f6
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2026-02-14 19:05:12 -0800

    Changed target sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.example.coffeescout"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
